98840cdacc14f952ad081a4aa7c4f7704203e8ae
SenkoVlad <vlad.senko2011@yandex.ru>
2021-07-01 17:57:04 +0300

    added dependency injections extantions

---
+++ CleanArchitecture.Application/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper.Configuration;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace CleanArchitecture.Application
+{
+    static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services,
+                                                IConfiguration configuration)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
---
+++ CleanArchitecture.Persistence/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using CleanArchitecture.Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Persistence
+{
+    static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services,
+                                                        IConfiguration configuration)
+        {
+            var connectionString = configuration["dbConnection"];
+            services.AddDbContext<NotedDbContext>(options =>
+                    options.UseSqlite(connectionString));
+            services.AddScoped<INotesDbContext>(provider =>
+                    provider.GetService<NotedDbContext>());
+
+            return services;
+        }
+    }
+}
